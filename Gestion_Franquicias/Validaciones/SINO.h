72f58376a2356ae0016ba22b260e70efb50dbb43
walterstamm <walterstamm92@gmail.com>
2021-02-14 23:30:42 -0300

    cambios

--- Gestion_Franquicias/Validaciones/SINO.h
+++
@@ -1,9 +0,0 @@
-#ifndef SINO_H_INCLUDED
-#define SINO_H_INCLUDED
-
-bool Continuar();
-bool Continuar_SinCinIgnore();
-bool SINO();///FUNCION QUE SOLO PREGUNTA SI O NO
-bool SINO_SinCinIgnore();
-
-#endif // SINO_H_INCLUDED
